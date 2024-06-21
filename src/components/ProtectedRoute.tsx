c0a02e2dae837e50fb1de04f7550c0519156a90f
JDRGit <jaimerodriguez85@gmail.com>
2024-06-20 22:55:11 -0400

    protectedroute updated

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,10 +1,14 @@
 "use client";
 
-import React, { useEffect } from 'react';
+import React, { useEffect, ReactNode } from 'react';
 import { useAuth } from '../context/AuthContext';
 import { useRouter } from 'next/navigation';
 
-const ProtectedRoute: React.FC = ({ children }) => {
+interface ProtectedRouteProps {
+  children: ReactNode;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
   const { user, loading } = useAuth();
   const router = useRouter();
 
